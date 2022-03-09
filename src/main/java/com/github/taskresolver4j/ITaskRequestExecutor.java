b507bcbeedd69585bcb301e4b108b15c5eaee15a
Leonardo de Lima Oliveira <l3onardo-oliv3ira@gmail.com>
2022-03-09 15:03:13 -0400

    simple

--- src/main/java/com/github/taskresolver4j/ITaskRequestExecutor.java
+++ src/main/java/com/github/taskresolver4j/ITaskRequestExecutor.java
@@ -3,7 +3,6 @@ package com.github.taskresolver4j;
 import com.github.taskresolver4j.exception.TaskExecutorException;
 
 public interface ITaskRequestExecutor<I, O> {
-  
   void execute(I request, O response) throws TaskExecutorException;
   void close();
 }
